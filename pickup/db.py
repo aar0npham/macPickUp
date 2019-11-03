44f85dda07080f14b28f493d552d7a5f26f64d6e
Aaron <aaronpham0103@gmail.com>
2019-11-02 21:00:15 -0400

    updates db

--- pickup/db.py
+++ pickup/db.py
@@ -2,14 +2,13 @@ from pymongo import MongoClient
 import json
 
 
-def load_mongo(db='MacPickUp'):
-    password = input('Password: ')
-    client = MongoClient(
-        "mongodb+srv://aar0npham:{}@food-izclc.mongodb.net/test?retryWrites=true&w=majority".format(password))
+def load_mongo(db='Restaurant'):
+    password = input('\nPassword: \n')
+    client = MongoClient("mongodb+srv://aar0npham:<password>@macpickup-izclc.mongodb.net/test?retryWrites=true&w=majority".format(password))
     return client[db]
 
 
-def write_db(col='Food', file='pickup.json'):
+def write_db(col='Menu', file='pickup.json'):
     dump = load_mongo()
     cur = dump[col].find({})
     count = dump[col].count_document({})
@@ -21,3 +20,6 @@ def write_db(col='Food', file='pickup.json'):
                 f.write(',')
         f.write(']')
     f.close()
+
+def get_data(col, places, restaurant):
+    pass
